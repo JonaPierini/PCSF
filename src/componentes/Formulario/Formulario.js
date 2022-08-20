1d7028eb1801a99227264ec5fab44b36205fa94a
Jona Pierini <jonapierini@gmail.com>
2022-08-20 15:27:42 -0300

    agregado form volver

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,59 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Formulario = () => {
-
-
   const [values, setValues] = useState({
-        nombre: '',
-        apellido: '',
-        email: '',
-  })
+    nombre: "",
+    apellido: "",
+    email: "",
+  });
 
-  const handleInputValue = (e) =>{
-        console.log(e.target.value)
-        console.log(e.target.name)
-        setValues({
-            ...values, //copio todas las propiedades que ya tenia el objeto => osea una string
-            [e.target.name] : e.target.value, // aca lo sobre escribe => asi es la sintaxis
-        })
-  }
+  const handleInputValue = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.name);
+    setValues({
+      ...values, //copio todas las propiedades que ya tenia el objeto => osea una string
+      [e.target.name]: e.target.value, // aca lo sobre escribe => asi es la sintaxis
+    });
+  };
 
-  const handleSubmit = (e) =>{
-      e.preventDefault()
-      console.log(values)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
 
   return (
     <>
-        <form  onSubmit={handleSubmit}>
-            <div  className='p-2 bg-light border'>
-                <input className='form-control'
-                    placeholder='Nombre'
-                    name='nombre'
-                    value={values.nombre}
-                    onChange={handleInputValue}>
-                </input>
-            </div>
-            <div  className='p-2 bg-light border'>
-                <input className='form-control'
-                    placeholder='Apellido'
-                    name='apellido'
-                    value={values.apellido}
-                    onChange={handleInputValue}>
-                </input>
-            </div>
-            <div className='p-2 bg-light border'>
-                <input className='form-control'
-                    placeholder='Email'
-                    name='email'
-                    value={values.email}
-                    onChange={handleInputValue}>
-                </input>
-            </div>
-            <div  className='d-grid gap-2 m-2'>
-                <button className='btn btn-primary'>Enviar</button>
-            </div>
-        </form> 
+      <form onSubmit={handleSubmit}>
+        <div className="p-2 bg-light border">
+          <input
+            className="form-control"
+            placeholder="Nombre"
+            name="nombre"
+            value={values.nombre}
+            onChange={handleInputValue}
+          ></input>
+        </div>
+        <div className="p-2 bg-light border">
+          <input
+            className="form-control"
+            placeholder="Apellido"
+            name="apellido"
+            value={values.apellido}
+            onChange={handleInputValue}
+          ></input>
+        </div>
+        <div className="p-2 bg-light border">
+          <input
+            className="form-control"
+            placeholder="Email"
+            name="email"
+            value={values.email}
+            onChange={handleInputValue}
+          ></input>
+        </div>
+        <div className="d-grid gap-2 m-2">
+          <button className="btn btn-primary">Enviar</button>
+        </div>
+        <Link to={"/"}>
+          <div className="d-grid gap-2 m-2">
+            <button className="btn btn-danger">Volver</button>
+          </div>
+        </Link>
+      </form>
     </>
   );
 };
